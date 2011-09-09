289f7849100cdadcd5a9994faac8a4c2ef452819
Dimitrios <amaxilat@cti.gr>
2011-09-09 11:25:12 +0300

    add scripts for tr

---
+++ generic/spitfire_demo/tr.scripts/scripts/listen.java
@@ -0,0 +1,117 @@
+import java.util.*;
+import java.nio.*;
+
+import javax.xml.datatype.DatatypeFactory;
+import javax.xml.datatype.XMLGregorianCalendar;
+
+import eu.wisebed.testbed.api.rs.RSServiceHelper;
+import eu.wisebed.api.rs.PublicReservationData;
+import eu.wisebed.api.rs.RS;
+
+import eu.wisebed.api.snaa.SNAA;
+import eu.wisebed.api.snaa.AuthenticationTriple;
+import eu.wisebed.api.snaa.SecretAuthenticationKey;
+import eu.wisebed.testbed.api.snaa.helpers.SNAAServiceHelper;
+
+import eu.wisebed.testbed.api.wsn.WSNServiceHelper;
+import eu.wisebed.api.controller.*;
+import eu.wisebed.api.common.*;
+import eu.wisebed.api.sm.*;
+import eu.wisebed.api.wsn.*;
+
+import de.uniluebeck.itm.tr.util.*;
+import de.itm.uniluebeck.tr.wiseml.WiseMLHelper;
+
+import de.uniluebeck.itm.wisebed.cmdlineclient.*;
+import de.uniluebeck.itm.wisebed.cmdlineclient.wrapper.*;
+import java.util.concurrent.Future;
+import com.google.common.collect.*;
+
+import de.uniluebeck.itm.wisebed.cmdlineclient.protobuf.*;
+
+
+
+//--------------------------------------------------------------------------
+// Configuration
+//--------------------------------------------------------------------------
+
+	// Endpoint URL of local controller instance, the testbed will use this URL to send us node outputs
+//	String localControllerEndpointURL	= "http://" + InetAddress.getLocalHost().getCanonicalHostName() + ":8091/controller";
+	String localControllerEndpointURL	= System.getProperty("local.endpoint")+":"+ System.getProperty("local.portlisten");
+	String secretReservationKeys = System.getProperty("testbed.secretreservationkeys");
+
+	// Endpoint URLs of Authentication (SNAA), Reservation (RS) and Experimentation (iWSN) services
+	String sessionManagementEndpointURL	= System.getProperty("testbed.sm.endpointurl");
+
+	// Retrieve Java proxies of the endpoint URLs above
+	SessionManagement sessionManagement = WSNServiceHelper.getSessionManagementService(sessionManagementEndpointURL);
+
+         private BufferedWriter out;
+
+
+//--------------------------------------------------------------------------
+// Application logic
+//--------------------------------------------------------------------------
+
+	Controller controller = new Controller() {
+		public void receive(List msgs) {
+			for (int i=0; i<msgs.size(); i++) {
+				//log.debug("received "+msg.size());
+				Message msg = (Message) msgs.get(i);
+				synchronized(System.out) {
+					String s = helper.toString(msg,true);
+                                        log.debug("Received message: "+s);
+                                        out.write(s+"\n");
+                                        out.flush();
+		            	}
+			}
+		}
+		public void receiveStatus(List requestStatuses) {
+			// nothing to do
+		}
+		public void receiveNotification(List msgs) {
+			for (int i=0; i<msgs.size(); i++) {
+				log.info(msgs.get(i));
+			}
+		}
+		public void experimentEnded() {
+			log.info("Experiment ended");
+			System.exit(0);
+		}
+	};
+
+	log.info("Using the following parameters for calling getInstance(): {}, {}",
+			StringUtils.jaxbMarshal(helper.parseSecretReservationKeys(secretReservationKeys)),
+			localControllerEndpointURL
+	);
+
+	DelegatingController delegator = new DelegatingController(controller);
+	delegator.publish(localControllerEndpointURL);
+
+                        out = new BufferedWriter(new FileWriter("trace2.out"));
+
+
+	log.info("Local controller published on url: {}", localControllerEndpointURL);
+
+	String wsnEndpointURL = null;
+	try {
+		wsnEndpointURL = sessionManagement.getInstance(
+				helper.parseSecretReservationKeys(secretReservationKeys),
+				localControllerEndpointURL
+		);
+	} catch (UnknownReservationIdException_Exception e) {
+		log.warn("There was not reservation found with the given secret reservation key. Exiting.");
+		System.exit(1);
+	}
+
+	log.info("Got a WSN instance URL, endpoint is: {}", wsnEndpointURL);
+	WSN wsnService = WSNServiceHelper.getWSNService(wsnEndpointURL);
+	final WSNAsyncWrapper wsn = WSNAsyncWrapper.of(wsnService);
+
+	while(true) {
+		try {
+			System.in.read();
+		} catch (Exception e) {
+			System.err.println(e);
+		}
+	}
