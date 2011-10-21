50d7574fbff4b3b0253caf5e03dedccf75e2d661
Dimitrios Amaxilatis <amaxilat@cti.gr>
2011-10-21 13:07:29 +0300

    add automatic node insertion based on testbed prefix

--- javaApps/csv2xml/src/main/java/eu/amaxilatis/csv2xml/WisedbUpdater.java
+++ javaApps/csv2xml/src/main/java/eu/amaxilatis/csv2xml/WisedbUpdater.java
@@ -2,7 +2,11 @@ package eu.amaxilatis.csv2xml;
 
 import eu.wisebed.wisedb.HibernateUtil;
 import eu.wisebed.wisedb.controller.NodeController;
+import eu.wisebed.wisedb.controller.TestbedController;
+import eu.wisebed.wisedb.model.Testbed;
+import eu.wisebed.wiseml.model.setup.Node;
 import eu.wisebed.wiseml.model.setup.Position;
+import eu.wisebed.wiseml.model.setup.Setup;
 import org.apache.log4j.Logger;
 import org.hibernate.Transaction;
 
@@ -15,10 +19,10 @@ import org.hibernate.Transaction;
 class WisedbUpdater {
     private static final Logger log = Logger.getLogger(WisedbUpdater.class);
 
-    private final Testbed testbed;
+    private final CsvTestbed csvTestbed;
 
-    WisedbUpdater(String testbed_name, Testbed testbed) {
-        this.testbed = testbed;
+    WisedbUpdater(String testbed_name, CsvTestbed csvTestbed) {
+        this.csvTestbed = csvTestbed;
         check();
     }
 
@@ -26,34 +30,70 @@ class WisedbUpdater {
         // Initialize hibernate
         HibernateUtil.connectEntityManagers();
 
-        for (String nodeId : testbed.getNodes()) {
+        for (String nodeId : csvTestbed.getNodes()) {
             log.debug(nodeId);
-            Node node = testbed.getNode(nodeId);
+            CsvNode csvNode = csvTestbed.getNode(nodeId);
             Transaction tx = HibernateUtil.getInstance().getSession().beginTransaction();
 
-            eu.wisebed.wiseml.model.setup.Node cnode = NodeController.getInstance().getByID(node.id());
+            Node cnode = NodeController.getInstance().getByID(csvNode.id());
             if (cnode != null) {
 
-                log.debug(node.id());
+                log.debug(csvNode.id());
 
 
                 Position p = cnode.getPosition();
                 try {
-                    p.setX(node.getX());
-                    p.setY(node.getY());
-                    p.setZ(node.getZ());
+                    p.setX(csvNode.getX());
+                    p.setY(csvNode.getY());
+                    p.setZ(csvNode.getZ());
                     cnode.setPosition(p);
 
 
-                    cnode.setDescription(node.getDescrioption());
+                    cnode.setDescription(csvNode.getDescrioption());
                     log.info("updating node " + cnode.getId());
                     log.debug(cnode.getId() + "," + cnode.getPosition().getX() + "," + cnode.getPosition().getY() + "," + cnode.getPosition().getZ());
                     log.debug(cnode.getDescription());
                 } catch (NullPointerException e) {
-                    log.error("getPosition returned null value for node " + node.id());
+
+                    log.error("getPosition returned null value for node " + csvNode.id());
                 }
             } else {
-                log.error("Cannot find node!!!");
+
+                log.info("node does not exist, trying to add...");
+
+
+                final String testbedPrefix = csvNode.id().substring(0, csvNode.id().indexOf("0"));
+                log.debug("searching for testbed by urn: " + testbedPrefix);
+                final Testbed tbed = TestbedController.getInstance().getByUrnPrefix(testbedPrefix);
+                if (tbed != null) {
+                    log.debug("need to place node in " + tbed.getId());
+                    final Setup setup = tbed.getSetup();
+                    if (tbed != null) {
+                        log.debug(" for setup " + setup.getId());
+                        Node newNode = new Node();
+                        newNode.setId(csvNode.id());
+                        newNode.setDescription(csvNode.getDescrioption());
+                        newNode.setGateway(csvNode.gateway());
+                        newNode.setNodeType(csvNode.nodeType());
+                        Position position = new Position();
+                        position.setX(csvNode.getX());
+                        position.setY(csvNode.getY());
+                        position.setZ(csvNode.getZ());
+                        position.setPhi(0);
+                        position.setTheta(0);
+                        newNode.setPosition(position);
+                        newNode.setProgramDetails("");
+                        newNode.setSetup(setup);
+                        NodeController.getInstance().add(newNode);
+                        log.info("added node " + newNode.toString());
+                    } else {
+                        log.error("no setup for testbed with id:" + tbed.getId());
+                    }
+                } else {
+                    log.error("no testbed for urnprefix:" + testbedPrefix);
+                }
+
+
             }
 
             tx.commit();
