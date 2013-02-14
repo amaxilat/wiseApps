b7091a2953548e1e15742f91cf93da7678d1e37a
Dimitrios Amaxilatis <amaxilat@cti.gr>
2013-02-14 12:07:47 +0200

    add config

---
+++ generic/clustering_test/configuration.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   config.h
+ * Author: amaxilatis
+ *
+ * Created on January 8, 2013, 12:50 PM
+ */
+
+#define CHANNEL 14
+
+// Replace the first Algorithm name with one from the list in comment
+//#define ECHO//ECHO ADAPTIVE
+#define ECHO //ECHO ADAPTIVE
+// Replace the first Algorithm name with one from the list in comment
+#define LCA //FRONTS MOCA LCA
+
+//#define ENABLE_UART_CL
+#define FAILURES_PERCENTAGE 0
+
+//#define VIRTUAL_RADIO
+//#define REMOTE_DEBUG
+
+//#define CHANGE_POWER
+#define DB 0
